7f81dcff8f884b80310561538b9d53bdb0ffaf40
Damien Raude-Morvan <drazzib@drazzib.com>
2013-04-04 01:50:32 +0200

    First try to integration a Convention into plugin.

--- src/main/groovy/com/drazzib/gradle/jcasgen/plugin/JCasGenPlugin.groovy
+++ src/main/groovy/com/drazzib/gradle/jcasgen/plugin/JCasGenPlugin.groovy
@@ -30,59 +30,71 @@ class JCasGenPlugin implements Plugin<Project> {
     private static final String GENERATE_GROUP = 'generate'
 
     void apply(Project project) {
-        project.plugins.apply(JavaPlugin)
-
-        project.configurations.add('jcasgen') {
-            visible = false
-            transitive = false
-            description = "The JCasGen libraries to be used for this project."
-        }
-
-        project.configurations.compile {
-            extendsFrom project.configurations.jcasgen
-        }
-
-        project.convention.plugins.java.sourceSets.all { SourceSet sourceSet ->
+        project.apply plugin: 'java'
+        project.convention.plugins.jcasgen = new JCasGenConvention(project);
+        project.sourceSets.all { SourceSet sourceSet ->
             setupJCasGenFor(sourceSet, project)
         }
     }
 
     private setupJCasGenFor(SourceSet sourceSet, Project project) {
+        def configName = (sourceSet.getName().equals(SourceSet.MAIN_SOURCE_SET_NAME) ? "jcasgen" : sourceSet.getName() + "Jcasgen")
+        project.configurations.add(configName) {
+            visible = false
+            transitive = false
+            description = "The JCasGen libraries to be used for this project."
+            extendsFrom = []
+        }
+
         insertJCasGenSourceDirectorySetInto(sourceSet, project)
 
+        // Wire task
         Task jcasgenTask = createJCasGenTaskFor(sourceSet, project)
-        project.tasks[sourceSet.compileJavaTaskName].dependsOn(jcasgenTask)
+
+        // Generate source code before java compile
+        String compileJavaTaskName = sourceSet.getCompileTaskName("java");
+        Task compileJavaTask = project.tasks.getByName(compileJavaTaskName);
+        compileJavaTask.dependsOn(jcasgenTask)
     }
 
     private insertJCasGenSourceDirectorySetInto(SourceSet sourceSet, Project project) {
-        def typesystemDir = "src/${sourceSet.name}/jcasgen"
+        def typesystemDir = "src/${sourceSet.name}/typesystem"
+
         sourceSet.convention.plugins.jcasgen = new JCasGenSourceDirectory(sourceSet.name, project.fileResolver)
-        sourceSet.java { srcDir generatedJavaDirFor(project, sourceSet) }
         sourceSet.jcasgen { srcDir typesystemDir }
         sourceSet.resources { srcDir typesystemDir }
+
+        // Add generated source dir to compilation source
+        sourceSet.java { srcDir generatedJavaDirFor(project, sourceSet) }
     }
 
     private Task createJCasGenTaskFor(SourceSet sourceSet, Project project) {
-        def jcasgenTask = project.tasks.add(taskName(sourceSet), JCasGenTask)
-
+        def taskName = taskName(sourceSet)
+        def jcasgenTask = project.tasks.add(taskName, JCasGenTask)
         jcasgenTask.group = GENERATE_GROUP
         jcasgenTask.description = "Generates code from the ${sourceSet.name} JCasGen typesystem."
-        jcasgenTask.outputDirectory = generatedJavaDirFor(project, sourceSet)
-        jcasgenTask.conventionMapping.defaultSource = { sourceSet.jcasgen }
-        jcasgenTask.conventionMapping.jcasgenClasspath = {
-            def jcasgenClassPath = project.configurations.jcasgen.copy()
-            jcasgenClassPath.transitive = true
-            jcasgenClassPath
+        jcasgenTask.conventionMapping.map("classpath") {
+            return sourceSet.getCompileClasspath()
+        }
+        /*jcasgenTask.conventionMapping.map("typeSystemDir") {
+            return project.convention.plugins.jcasgen.typeSystemDir
+        }*/
+        jcasgenTask.conventionMapping.map('source') {
+            return sourceSet.jcasgen
+        }
+        jcasgenTask.conventionMapping.map('destinationDir') {
+            return generatedJavaDirFor(project, sourceSet)
         }
 
         jcasgenTask
     }
 
     private File generatedJavaDirFor(Project project, SourceSet sourceSet) {
-        project.file("${project.buildDir}/generated-src/jcasgen/${sourceSet.name}")
+        def generatedSourceDir = 'generated-src'
+        project.file("${project.buildDir}/${generatedSourceDir}/jcasgen/${sourceSet.name}")
     }
 
     private String taskName(SourceSet sourceSet) {
-        return sourceSet.getTaskName('generate', 'SchemaSource')
+        return sourceSet.getTaskName('generate', 'TypeSystem')
     }
 }
