2099005e2e3f986d4eb7f2b4aca9bd62be9f1d25
Damien Raude-Morvan <drazzib@drazzib.com>
2013-06-13 22:54:06 +0200

    Create a sample 'simple' project for jcasgen plugin.

---
+++ examples/simple/src/main/java/test/Sample.java
@@ -0,0 +1,12 @@
+package test;
+
+import test.Type1;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        // Try to create new Type1 item
+        Type1 type = new Type1();
+    }
+
+}
